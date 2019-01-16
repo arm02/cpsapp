177b9038d15d572fee33241c8f89cd8509e06315
Firman Kurniawan <mrroz3r@gmail.com>
2019-01-16 20:01:39 +0700

    Webcam + Jam Masuk

--- app/Http/Controllers/WebcamController.php
+++ app/Http/Controllers/WebcamController.php
@@ -6,20 +6,27 @@ use Illuminate\Http\Request;
 use Illuminate\Http\File;
 use Illuminate\Support\Facades\Storage;
 use \App\Webcam;
+use Carbon\Carbon;
 class WebcamController extends Controller
 {
+    public function index()
+    {
+        return view('webcam.index');
+    }
+
     public function save(Request $r)
     {
     	$nama = $r->nama;
     	$image = $r->input('gambar');
         $image = str_replace('data:image/jpeg;base64,','', $image);
         $image = base64_decode($image);
-        $filename = 'image_'.time().'.png';
+        $filename = 'webcam_'.uniqid().'.png';
         file_put_contents('uploads/'.$filename,$image);
+        $now = Carbon::now();
         $webcam = new \App\CekAbsensi;
         $webcam->nama = $nama;
         $webcam->foto = $filename;
-        $webcam->jammasuk = '123';
+        $webcam->jammasuk = \Carbon\Carbon::now()->format('d F Y, H:i');
         $webcam->save();
 
         return redirect(url('/'));
